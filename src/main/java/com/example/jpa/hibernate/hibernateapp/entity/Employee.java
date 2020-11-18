eaa7bc76fc8ad2e5a6926bb9e9c8b13df50fadc1
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-11-18 15:47:15 +0530

    Add Employee entity

---
+++ src/main/java/com/example/jpa/hibernate/hibernateapp/entity/Employee.java
@@ -0,0 +1,46 @@
+package com.example.jpa.hibernate.hibernateapp.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public abstract class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Employee() {
+    }
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
